ae3c76f4ffaa49ff81aeb6bedc8fb68f48dedc24
cicidi <cicidi@gmail.com>
2017-03-14 23:54:19 -0700

    Feature-rm latest job/change button background

--- src/main/java/com/cicidi/home/controller/linkedin/LinkedinPhotosController.java
+++ src/main/java/com/cicidi/home/controller/linkedin/LinkedinPhotosController.java
@@ -1,4 +1,4 @@
-package com.cicidi.home.controller.linkedin;///*
+ package com.cicidi.home.controller.linkedin;///*
 // * Copyright 2014 the original author or authors.
 // *
 // * Licensed under the Apache License, Version 2.0 (the "License");
