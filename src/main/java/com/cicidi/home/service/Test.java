6098d8c154b02626b145345ad2ee0928e9367be1
cicidi <cicidi1985>
2017-04-11 09:18:44 -0700

    Fix-Pic size

--- src/main/java/com/cicidi/home/service/Test.java
+++ src/main/java/com/cicidi/home/service/Test.java
@@ -1,22 +1,5 @@
 package com.cicidi.home.service;
 
-import com.cicidi.home.domain.resume.*;
-import com.cicidi.home.domain.resume.Link;
-import com.cicidi.home.domain.vo.WebLog;
-import com.cicidi.home.io.ApacheXML2PDF;
-import com.cicidi.home.io.XMLReader;
-import com.cicidi.home.util.Constants;
-import org.eclipse.egit.github.core.RepositoryCommit;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.xml.bind.JAXBException;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 /**
  * Created by cicidi on 2/18/17.
  */
